46e4098254177619b99c7602c598dc7b617867fa
Thomas Bury <tombury182@gmail.com>
2018-11-13 10:53:24 -0500

    additive noise folder

---
+++ additive_noise/single_trans.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 13 10:49:48 2018
+
+@author: tb460
+
+Script to simluate a single transient trajectory of
+May's harvesting model with additive noise, and compute EWS.
+
+"""
+
+
+
+
+
+
