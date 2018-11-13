9412f0253370f5aa5ae6e35c52bf078c651608bd
Thomas Bury <tombury182@gmail.com>
2018-11-13 12:55:23 -0500

    model parameters

--- additive_noise/single_trans.py
+++ additive_noise/single_trans.py
@@ -10,6 +10,32 @@ May's harvesting model with additive noise, and compute EWS.
 
 """
 
+# import python libraries
+import numpy as np
+import pandas as pd
+
+# import EWS function
+import sys
+sys.path.append('../../early_warnings')
+from ews_compute import ews_compute
+
+
+# parameters for simulation
+dt = 1
+tmax = 100
+
+
+# model parameters
+a1 = 0.02 # noise intensity
+r = 1
+s = 0.1
+hl = 0.14 # min value of control param
+hh = 0.28 # max value of control param
+x0 = 0.89 # intial condition at equilibrium (from MMA)
+
+
+
+
 
 
 
